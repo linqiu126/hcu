50b145fa5f587d7657f8af9148e31760a8c1c9bd
linqiu126 <linqiu126@gmail.com>
2016-06-11 16:25:08 +0800

    20160611 first version .gitignore which contains serverl decided directories

--- Debug/src/l0webser/get_time.d
+++
@@ -1,4 +0,0 @@
-src/l0webser/get_time.o: ../src/l0webser/get_time.c \
- ../src/l0webser/../l0webser/get_time.h
-
-../src/l0webser/../l0webser/get_time.h:
--- Debug/src/l3app/sensorwindspeed.d
+++
@@ -1,1 +0,0 @@
-src/L3App/sensorwindspeed.o: ../src/L3App/sensorwindspeed.c
