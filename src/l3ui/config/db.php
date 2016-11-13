6262fe325d1c44301de75670213340571e240672
smdzjl <smdzjl@sina.cn>
2016-11-14 01:51:54 +0800

    zjl@2016/11/13 R01.115.u4

--- src/l3ui/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=hcudb',
-    'username' => 'testuser',
-    'password' => '123456',
-    'charset' => 'utf8',
-];
