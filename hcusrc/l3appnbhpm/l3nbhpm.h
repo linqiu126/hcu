b4249dd16e8eca4f2f21da921e94c2495cc92fc2
smdzjl <smdzjl@sina.cn>
2016-11-14 16:49:23 +0800

    zjl@2016/11/14 R01.119

---
+++ hcusrc/l3appnbhpm/l3nbhpm.h
@@ -0,0 +1,17 @@
+/*
+ * l3nbhpm.h
+ *
+ *  Created on: 2015年11月22日
+ *      Author: test
+ */
+
+#ifndef L3APP_NBIOT_HPM_H_
+#define L3APP_NBIOT_HPM_H_
+
+#include "../l0comvm/vmlayer.h"
+#include "../l1com/l1comdef.h"
+
+
+
+
+#endif /* L3APP_NBIOT_HPM_H_ */
