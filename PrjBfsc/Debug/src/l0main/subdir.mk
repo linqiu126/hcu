bb536fdca04cecc5c1236d83f20ac4c101b65e99
smdzjl <smdzjl@sina.cn>
2016-11-14 14:01:38 +0800

    zjl@2016/11/14 R01.117

--- PrjBfsc/Debug/src/l0main/subdir.mk
+++ PrjBfsc/Debug/src/l0main/subdir.mk
@@ -4,27 +4,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/home/hitpony/workspace/hcu/hcusrc/l0main/hcu.c \
-/home/hitpony/workspace/hcu/hcusrc/l0main/hcuref.c 
+/home/hitpony/workspace/hcu/hcusrc/l0main/hcu.c 
 
 OBJS += \
-./src/l0main/hcu.o \
-./src/l0main/hcuref.o 
+./src/l0main/hcu.o 
 
 C_DEPS += \
-./src/l0main/hcu.d \
-./src/l0main/hcuref.d 
+./src/l0main/hcu.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/l0main/hcu.o: /home/hitpony/workspace/hcu/hcusrc/l0main/hcu.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: Cross GCC Compiler'
-	gcc -DTARGET_LINUX_X86 -I/usr/include/libxml2 -I/usr/include/curl -I/root/ffmpeg_build/include -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-src/l0main/hcuref.o: /home/hitpony/workspace/hcu/hcusrc/l0main/hcuref.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
 	gcc -DTARGET_LINUX_X86 -I/usr/include/libxml2 -I/usr/include/curl -I/root/ffmpeg_build/include -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
