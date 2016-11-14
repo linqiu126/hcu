a3f4ab46b328a28e8f5c657f15d47490d1f6eaaa
smdzjl <smdzjl@sina.cn>
2016-11-14 11:58:34 +0800

    zjl@2016/11/14 R01.116 u1

---
+++ PrjCxGlacm/Linux_Arm_A8/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lrt -lmysqlclient -lpthread -lxml2 -lcurl -lavcodec -lavfilter -lfdk-aac -lx264 -lpostproc -lavformat -lavutil -lavdevice -lavfilter -lswscale -lswresample -lm -lsqlite3 -lncurses -lstdc++ -lc
+
