7544f85be8da5dad57973636c49de63ae38fd2dd
test <hitpony@qq.com>
2017-06-10 17:48:47 +0800

    zjl@2017/6/11 R03.207

--- hcusrc/l2frame/canitfleo.c
+++ hcusrc/l2frame/canitfleo.c
@@ -684,9 +684,15 @@ OPSTAT func_canitfleo_l2frame_msg_bfsc_startup_ind_received_handle(StrMsg_HUITP_
 	//因为没有标准的IE结构，所以这里不能再验证IEID/IELEN的大小段和长度问题
 
 	//系统初始化过程
-	if (FsmGetState(TASK_ID_L3BFSC) < FSM_STATE_L3BFSC_OOS_SCAN){
+	if (FsmGetState(TASK_ID_L3BFSC) == FSM_STATE_L3BFSC_ACTIVED){
 		gTaskL3bfscContext.sensorWs[nodeId].sensorStatus = HCU_L3BFSC_SENSOR_WS_STATUS_STARTUP;
 		HCU_DEBUG_PRINT_CRT("CANITFLEO: Sensor ID = %d is set to be startup!\n", nodeId);
+	}
+
+	else if (FsmGetState(TASK_ID_L3BFSC) == FSM_STATE_L3BFSC_OPR_CFG){
+		if (gTaskL3bfscContext.sensorWs[nodeId].sensorStatus != HCU_L3BFSC_SENSOR_WS_STATUS_CFG_REQ){
+			gTaskL3bfscContext.sensorWs[nodeId].sensorStatus = HCU_L3BFSC_SENSOR_WS_STATUS_OFFLINE;
+		}
 	}
 
 	//系统已经在工作状态：发送CFG给IHU
