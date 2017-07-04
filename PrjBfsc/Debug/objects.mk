a8436df120735338a6d4d631979888c3ee619f90
hitpony <hitpony@qq.com>
2017-07-04 21:32:38 +0800

    LZH@2017/07/04 open json and inotify

--- PrjBfsc/Debug/objects.mk
+++ PrjBfsc/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lrt -lmysqlclient -lpthread -lcurl -lxml2 -lavcodec -lavformat -lavutil -lavdevice -lavfilter -lswscale -lm -lSDL2 -lsqlite3
+LIBS := -lrt -ljson-c -lmysqlclient -lpthread -lcurl -lxml2 -lavcodec -lavformat -lavutil -lavdevice -lavfilter -lswscale -lm -lSDL2 -lsqlite3
 
