da5b7a3027b64b6bb4b55746198864587b78077f
smdzjl <smdzjl@sina.cn>
2017-09-05 06:55:39 +0000

    compilet&link pass for MQTT & Jason in RasPI by Shanchun

--- PrjAqycG20/RasberryPi/src/l0service/subdir.mk
+++ PrjAqycG20/RasberryPi/src/l0service/subdir.mk
@@ -6,6 +6,7 @@
 C_SRCS += \
 /home/pi/workspace/hcu/hcusrc/l0service/configxml.c \
 /home/pi/workspace/hcu/hcusrc/l0service/mmc.c \
+/home/pi/workspace/hcu/hcusrc/l0service/mqtt.c \
 /home/pi/workspace/hcu/hcusrc/l0service/sysinfo.c \
 /home/pi/workspace/hcu/hcusrc/l0service/timer.c \
 /home/pi/workspace/hcu/hcusrc/l0service/trace.c 
@@ -13,6 +14,7 @@ C_SRCS += \
 OBJS += \
 ./src/l0service/configxml.o \
 ./src/l0service/mmc.o \
+./src/l0service/mqtt.o \
 ./src/l0service/sysinfo.o \
 ./src/l0service/timer.o \
 ./src/l0service/trace.o 
@@ -20,6 +22,7 @@ OBJS += \
 C_DEPS += \
 ./src/l0service/configxml.d \
 ./src/l0service/mmc.d \
+./src/l0service/mqtt.d \
 ./src/l0service/sysinfo.d \
 ./src/l0service/timer.d \
 ./src/l0service/trace.d 
@@ -34,6 +37,13 @@ src/l0service/configxml.o: /home/pi/workspace/hcu/hcusrc/l0service/configxml.c
 	@echo ' '
 
 src/l0service/mmc.o: /home/pi/workspace/hcu/hcusrc/l0service/mmc.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -DTARGET_RASPBERRY_PI3B -I/usr/include/libxml2 -I/usr/include/curl -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/l0service/mqtt.o: /home/pi/workspace/hcu/hcusrc/l0service/mqtt.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
 	gcc -DTARGET_RASPBERRY_PI3B -I/usr/include/libxml2 -I/usr/include/curl -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
