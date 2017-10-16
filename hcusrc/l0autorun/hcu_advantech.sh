425d4634f699dcf0185e24c1e7158bffc345ec61
hitpony <smdzjl@sina.cn>
2017-10-16 20:41:06 +0800

    zjl@2017/10/16 R03.280

--- hcusrc/l0autorun/hcu_advantech.sh
+++ hcusrc/l0autorun/hcu_advantech.sh
@@ -45,21 +45,34 @@ do_start()
 		sleep 10
 		sudo cd /var/hcu
 		if [ -f "/var/hcu/hcu_new" ]; then 
-			#执行顺序
-			#先tar -uzip
-			#然后将界面目录替换性拷贝到目标 /var/www/html/localui/
-			#将hcu_new中的执行文件拷贝覆盖hcu执行程序
-			#最终清理hcu_new以及tar -unzip的现场垃圾文件
-
+			#unzip hcu_new
+			unzip /var/hcu/hcu_new -d /var/hcu/temp
+			
+			#hcu program part
+			if [ -f "/var/hcu/temp/hcu" ]; then
+				rm /var/hcu/hcu
+				cp /var/hcu/temp/hcu /var/hcu/hcu
+				chmod 777 /var/hcu/hcu
+				echo "$(date +%Y-%m-%d_%H:%M:%S)  hcu upgraded."
+			fi
+			
+			#localui part
+			if [ -d "/var/hcu/temp/localui" ]; then
+			  rm -r /var/www/html/localui
+			  cp -r /var/hcu/temp/localui /var/www/html/localui
+			  echo "$(date +%Y-%m-%d_%H:%M:%S)  localui upgraded."
+			fi
+			
+			rm -r /var/hcu/temp
+			echo "$(date +%Y-%m-%d_%H:%M:%S)  directory /var/hcu/temp removed."		
+		
 			#先停止hcu服务
 			echo "$(date +%Y-%m-%d_%H:%M:%S)  hcu_new exits."
 			#sudo systemctl stop hcu
 			#echo "$(date +%Y-%m-%d_%H:%M:%S)  hcu service stopped."
-			sudo rm /var/hcu/hcu
-			echo "$(date +%Y-%m-%d_%H:%M:%S)  old hcu removed."
-			sudo mv /var/hcu/hcu_new /var/hcu/hcu
-			sudo chmod 777 /var/hcu/hcu
-			echo "$(date +%Y-%m-%d_%H:%M:%S)  new hcu replaced."
+			#sudo rm /var/hcu/hcu
+			#sudo mv /var/hcu/hcu_new /var/hcu/hcu
+			#sudo chmod 777 /var/hcu/hcu
 			#sudo systemctl start hcu
 			#echo "$(date +%Y-%m-%d_%H:%M:%S)  hcu service started."
 		fi
