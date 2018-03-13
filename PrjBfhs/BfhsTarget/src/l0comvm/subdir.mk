684016fd03dd5679bf96bc90ed048b5fd3504e80
hitpony <liuzehong@hotmail.com>
2018-03-13 21:13:50 +0800

    lzh@2018/3/13 update BFHS and DB

--- PrjBfhs/BfhsTarget/src/l0comvm/subdir.mk
+++ PrjBfhs/BfhsTarget/src/l0comvm/subdir.mk
@@ -20,14 +20,14 @@ C_DEPS += \
 src/l0comvm/vm_config.o: /home/hitpony/workspace/hcu/hcusrc/l0comvm/vm_config.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	gcc -DTARGET_LINUX_X86 -DTARGET_LINUX_X86_ADVANTECH -I/usr/include/libxml2 -I/usr/include/x86_64-linux-gnu/ -I/usr/include -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -Wl,--hash-style=sysv -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -DTARGET_LINUX_X86 -DTARGET_LINUX_X86_ADVANTECH -I/usr/include/libxml2 -I/usr/include/x86_64-linux-gnu/ -I/usr/include -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -Wl,--hash-style=sysv -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/l0comvm/vmlayer.o: /home/hitpony/workspace/hcu/hcusrc/l0comvm/vmlayer.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	gcc -DTARGET_LINUX_X86 -DTARGET_LINUX_X86_ADVANTECH -I/usr/include/libxml2 -I/usr/include/x86_64-linux-gnu/ -I/usr/include -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -Wl,--hash-style=sysv -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -DTARGET_LINUX_X86 -DTARGET_LINUX_X86_ADVANTECH -I/usr/include/libxml2 -I/usr/include/x86_64-linux-gnu/ -I/usr/include -I/usr/local/sqlite3/include -O0 -lpthread -g3 -Wall -c -fmessage-length=0 -Wl,--hash-style=sysv -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- hcusrc/l0dbi/dbi_bfhs.h
+++ hcusrc/l0dbi/dbi_bfhs.h
@@ -11,7 +11,7 @@
 #include "../l0comvm/vmlayer.h"
 
 #define DBI_BFHS_DATA_SAVE_DAYS_MIN 90  //最短90天，不能再短
-#define HCU_SYSCFG_BFHS_DB_COLUMN_NUM_MAX 24
+#define HCU_SYSCFG_BFHS_DB_COLUMN_NUM_MAX 27
 #define HCU_PRODUCTCFG_BFHS_DB_COLUMN_NUM_MAX 28
 
 //APIs
